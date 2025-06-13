f3e2863320c76b4214dc9bb0d7a1f9e614ddbd59
Youngpuffy <levimuturi2@gmail.com>
2025-06-13 10:45:23 +0300

    creates book model

--- lib/models.py
+++ lib/models.py
@@ -12,7 +12,17 @@ class Author(db.Model, SerializerMixin):
     name = db.Column(db.String(100),nullable=False)
 
     books = db.relationship('Book', backref='author', lazy= True)
-    
 
+class Book(db.Model, SerializerMixin):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    publication_year =db.Column(db.Integer, nullable=False)
+
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+
+    reviews = db.relationship('Review', secondary=book_review, backref=db.backref('books', lazy='dynamic'),lazy='dynamic')
+    
 
 
